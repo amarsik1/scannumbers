d8bf5132edc7c0b741b0c5a9dff19f8e4d786be4
amarsik1 <artan9889@gmail.com>
2021-01-08 19:16:03 +0200

    refactoring cors

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ const app = express();
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3012;
 
-require('./middleware/cors')(app);
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 require('./config/routes')(app);
 
 app.get('/', function (req, res) {
