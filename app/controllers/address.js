87e4ba3f0964018de6f5b540169af9d8b5d72d9e
amarsik1 <artan9889@gmail.com>
2021-01-08 20:24:54 +0200

    edit db path

--- app/controllers/address.js
+++ app/controllers/address.js
@@ -1,4 +1,4 @@
-const pool = require('../../db');
+const pool = require('../../config/db');
 
 const getAll = async (req, res) => {
     try {
--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -1,4 +1,4 @@
-const pool = require('../../db');
+const pool = require('../../config/db');
 const bCrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const {jwtSecret} = require('../../config/app');
