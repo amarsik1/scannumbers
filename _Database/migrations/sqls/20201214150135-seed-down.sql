fcc8395501682fcf29ec663d0ac8199b9c2a8ff3
amarsik1 <artan9889@gmail.com>
2021-01-08 19:45:34 +0200

    refactoring migrations code

--- _Database/migrations/sqls/20201214150135-seed-down.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE address;
-DROP TABLE street_type;
-DROP TABLE organization;
-DROP TABLE resource_type;
-DROP TABLE consumer;
-DROP TABLE meters_group;
-DROP TABLE meter;
---
+++ migrations/sqls/20201214150135-seed-down.sql
@@ -0,0 +1,7 @@
+DROP TABLE meter;
+DROP TABLE meters_group;
+DROP TABLE consumer;
+DROP TABLE organization;
+DROP TABLE address;
+DROP TABLE street_type;
+DROP TABLE resource_type;
