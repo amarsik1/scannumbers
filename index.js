204569ee937599221416213fb38c5f3cad1001d6
amarsik1 <artan9889@gmail.com>
2021-01-08 19:25:59 +0200

    install cors

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 const PORT = process.env.PORT || 3012;
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-    next();
-});
 
 require('./config/routes')(app);
 
