982e1355340a42c9f3df13d96b9009a904606c08
amarsik1 <artan9889@gmail.com>
2021-01-08 20:24:10 +0200

    edit db config

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'amarsik',
-    database: 'meters',
-    host: 'localhost',
+    user: 'exqjhahjxhxgpi',
+    password: 'c52b707e33087fb0c1646860c77b85f4734aeee4786702d703640f172fb0a6c3',
+    database: 'dcouv2s8q4a3eh',
+    host: 'ec2-52-44-139-108.compute-1.amazonaws.com',
     port: '5432'
 });
 
