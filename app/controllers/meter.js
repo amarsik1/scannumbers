eebebe42254674d370e666731479827a23c31892
amarsik1 <artan9889@gmail.com>
2021-03-02 21:09:16 +0200

    added meterService

--- app/controllers/meter.js
+++ app/controllers/meter.js
@@ -1,60 +1,39 @@
-const pool = require('../../config/db'),
-    {isExist: metersGroupIsExist} = require('./metersGroup'),
-    {isExist: organizationIsExist} = require('./organization'),
-    {validateMeter} = require('../models/meter.model');
-
-const isExist = async (_id) => {
-    const allMeterData = await pool.query("SELECT * FROM meter where meter_id = $1", [_id]);
-    return !!allMeterData.rows.length
-};
-
-const isValueExist = async (meter) => {
-    const {resource_type_id, organization_id, meters_group_id} = meter;
-    const meterInDb = await pool.query(
-        "SELECT * FROM meter where resource_type_id = $1 and organization_id = $2 and meters_group_id = $3",
-        [resource_type_id, organization_id, meters_group_id]
-    );
-    return meterInDb.rows
-};
+const {validateMeter} = require('../models/meter.model');
+const metersGroupService = require('../servise/metersGroup.service');
+const organizationService = require('../servise/organization.service');
+const meterService = require('../servise/meter.service');
 
 const create = async (req, res) => {
-    try {
-        const {error} = validateMeter(req.body);
+    const {error} = validateMeter(req.body);
 
-        if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send(error.details[0].message);
 
-        const meterInDb = await isValueExist(req.body);
-        if (!!meterInDb.length) return res.status(400).send('Meter with this value already exists');
-        const {personal_account, resource_type_id, organization_id, meters_group_id} = req.body;
+    const meterInDb = await meterService.isValueExist(req.body);
+    if (!!meterInDb.length) return res.status(400).send('Meter with this value already exists');
+    const {organization_id, meters_group_id} = req.body;
 
-        if (!await metersGroupIsExist(meters_group_id)) return res.status(400).send('Meter group does not exists');
-        if (!await organizationIsExist(organization_id)) return res.status(400).send('Organization does not exists');
+    if (!await metersGroupService.isExist(meters_group_id)) return res.status(400).send('Meter group does not exists');
+    if (!await organizationService.isExist(organization_id)) return res.status(400).send('Organization does not exists');
 
-        const newMeter = await pool.query("INSERT INTO meter (personal_account, resource_type_id, organization_id, meters_group_id) VALUES ($1,$2,$3,$4) RETURNING *",
-            [personal_account, resource_type_id, organization_id, meters_group_id]);
+    const newMeter = meterService.create(req.body)
 
-        res.status(200).send(newMeter.rows[0]);
-    } catch (err) {
-        res.status(500).send(err)
-    }
+    res.status(200).send(newMeter);
 };
 
 const update = async (req, res) => {
     const {error} = validateMeter(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-    const {personal_account, resource_type_id, organization_id, meters_group_id} = req.body;
-    const {id} = req.params;
+    const {id, organization_id, meters_group_id} = req.body;
 
-    if (!await metersGroupIsExist(meters_group_id)) return res.status(400).send('Meter group does not exists');
-    if (!await organizationIsExist(organization_id)) return res.status(400).send('Organization does not exists');
-    if (!await isExist(id)) return res.status(400).send('Meter does not exists');
+    if (!await metersGroupService.isExist(meters_group_id)) return res.status(400).send('Meter group does not exists');
+    if (!await organizationService.isExist(organization_id)) return res.status(400).send('Organization does not exists');
+    if (!await meterService.isExist(id)) return res.status(400).send('Meter does not exists');
 
-    const meterInDb = await isValueExist(req.body);
+    const meterInDb = await meterService.isValueExist(req.body);
     if (!!meterInDb.length)
         if (meterInDb[0].meter_id !== +id) return res.status(400).send('Meter with this value already exists');
 
-    await pool.query("UPDATE meter SET personal_account = $1, resource_type_id = $2, organization_id = $3, meters_group_id = $4 where meter_id = $5",
-        [personal_account, resource_type_id, organization_id, meters_group_id, id]);
+    await meterService.update(req.body);
 
     res.status(200).send("Meters group was successfully updated");
 };
@@ -62,15 +41,13 @@ const update = async (req, res) => {
 const deleteOne = async (req, res) => {
     const {id} = req.params;
 
-    if (!await isExist(id)) return res.status(400).send('Meter does not exists');
-    await pool.query('delete from meter where meter_id = $1', [id]);
-
+    if (!await meterService.isExist(id)) return res.status(400).send('Meter does not exists');
+    await meterService.deleteOne(id)
     res.status(200).send('Successfully deleted');
 
 };
 
 module.exports = {
-    isExist,
     create,
     update,
     deleteOne
---
+++ app/servise/meter.service.js
@@ -0,0 +1,40 @@
+const pool = require('../../config/db');
+
+const isExist = async (id) => {
+    const allMeterData = await pool.query("SELECT * FROM meter where meter_id = $1", [id]);
+    return !!allMeterData.rows.length;
+};
+
+const isValueExist = async (meter) => {
+    const {resource_type_id, organization_id, meters_group_id} = meter;
+    const meterInDb = await pool.query(
+        "SELECT * FROM meter where resource_type_id = $1 and organization_id = $2 and meters_group_id = $3",
+        [resource_type_id, organization_id, meters_group_id]
+    );
+    return meterInDb.rows;
+};
+
+const create = async (meter) => {
+    const {personal_account, resource_type_id, organization_id, meters_group_id} = meter;
+    const newMeter = await pool.query("INSERT INTO meter (personal_account, resource_type_id, organization_id, meters_group_id) VALUES ($1,$2,$3,$4) RETURNING *",
+        [personal_account, resource_type_id, organization_id, meters_group_id]);
+    return newMeter.rows[0];
+};
+
+const update = async (meter) => {
+    const {id, personal_account, resource_type_id, organization_id, meters_group_id} = meter;
+    await pool.query("UPDATE meter SET personal_account = $1, resource_type_id = $2, organization_id = $3, meters_group_id = $4 where meter_id = $5",
+        [personal_account, resource_type_id, organization_id, meters_group_id, id]);
+};
+
+const deleteOne = async (id) => {
+    await pool.query('delete from meter where meter_id = $1', [id]);
+};
+
+module.exports = meterServise = {
+    isExist,
+    isValueExist,
+    create,
+    update,
+    deleteOne
+}
