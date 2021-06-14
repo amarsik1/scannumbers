8039c98b16a715c986cb7d3abddee5328a8f9bcb
Artem <32891609+amarsik1@users.noreply.github.com>
2021-06-15 00:45:16 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,14 +4,14 @@ const {jwtSecret} = require('../config/app');
 module.exports = (req, res, next) => {
     const authHeader = req.get('Authorization');
     if (!authHeader) {
-        res.status(401).send('Token not provided');
+        res.status(401).send({message: 'Token not provided'});
     }
     const token = authHeader.replace('Bearer ', '');
     try {
         jwt.verify(token, jwtSecret);
     } catch (err) {
         if (err instanceof jwt.JsonWebTokenError) {
-            res.status(401).send('Invalid token');
+            res.status(401).send({message: 'Invalid token'});
         }
     }
     next();
